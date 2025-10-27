c8959bf3324feb31c9081f7c800d17e40e3ba782
samuellimabraz <samuellimabraz@gmail.com>
2025-10-26 21:40:35 -0300

    refactor: enable depth and rgb camera in vslam node and better rviz

--- launch/isaac_vslam_nodes.py
+++ launch/isaac_vslam_nodes.py
@@ -18,8 +18,8 @@ def generate_launch_description():
         parameters=[{
             'enable_infra1': True,
             'enable_infra2': True,
-            'enable_color': False,
-            'enable_depth': False,
+            'enable_color': True,
+            'enable_depth': True,
             'depth_module.emitter_enabled': 0,
             'depth_module.profile': '640x360x90',
             'enable_gyro': True,
